8553e06a75e203691d2e7457b2b0913c4578cdd4
jay <jay@localhost.localdomain>
2021-03-04 00:42:14 +0400

    cretaed sample array

---
+++ src/com/games/Main.java
@@ -0,0 +1,12 @@
+package com.games;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Array test = new Array(4);
+	  test.insert(10);
+	  test.insert(20);
+	  test.insert(30);
+        test.print();
+    }
+}
